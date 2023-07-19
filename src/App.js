6f0aeda3c38083be4651a3aaf5f6588816967828
HumblexTempServer <139093283+HumblexServerTemp@users.noreply.github.com>
2023-07-19 18:38:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/services" element={<Services />} />
-            <Route path="/bookservice" element={<BookService />} />
+            <Route path="/book" element={<BookService />} />
             <Route path="/review" element={<Review />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/faq" element={<FAQ />} />
